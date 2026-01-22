d6cda2411328e881039991c815a1e9681908fa5f
Martin Kalcok <martin.kalcok@canonical.com>
2026-01-22 14:43:10 +0100

    ovn: Make Prometheus tests skippable.
    When env. variable 'TEST_SKIP_PROMETHEUS' is exported, the prometheus exporter
    test is skipped.
    
    This allows older releases of OVN charms to use stable/caracal branch of
    zaza-openstack-tests and benefit from the Juju3 support.
    
    Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>

--- zaza/openstack/charm_tests/ovn/tests.py
+++ zaza/openstack/charm_tests/ovn/tests.py
@@ -15,6 +15,7 @@
 """Encapsulate OVN testing."""
 
 import logging
+import os
 import unittest
 
 import juju
@@ -229,6 +230,10 @@ class BaseCharmOperationTest(test_utils.BaseCharmTest):
 
     def test_prometheus_exporter_reachable(self):
         """Ensure that the OVS/OVN exporter responds to requests."""
+        if os.environ.get('TEST_SKIP_PROMETHEUS') is not None:
+            self.skipTest('Prometheus testing skipped becuase '
+                          'TEST_SKIP_PROMETHEUS env is set.')
+
         for unit in zaza.model.get_units(self.application_name):
             ip = zaza.model.get_unit_public_address(unit)
             response = requests.get(
